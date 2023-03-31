e2be2c50df5b7a167399a4fe21168204c802c2db
satodoc <70416812+satodoc@users.noreply.github.com>
2023-04-01 03:12:20 +0900

    Update output-funcio-list.js

--- azure/functions/output-funcio-list.js
+++ azure/functions/output-funcio-list.js
@@ -3,14 +3,12 @@
 /**
  * 関数一覧を出力する
  */
-
-const path = require("path");
 // 関数名(=ディレクトリ名)をキーに入出力バインド定義のオブジェクトに変換
 const funcs = require("glob")
   .sync(`*/function.json`)
   .reduce((funcs, funcJsonPath) => {
-    const name = path.basename(path.dirname(`${ROOT_DIR}/${funcJsonPath}`));
-    const func = require(funcJsonPath);
+    const name = require("path").dirname(funcJsonPath);
+    const func = require(`./${funcJsonPath}`);
     funcs[name] = {
       in: func.bindings.find((bind) => bind.direction === "in"),
       out: func.bindings.filter((bind) => bind.direction === "out"),
@@ -18,8 +16,15 @@ const funcs = require("glob")
     return funcs;
   }, {});
 // 出力は適宜加工
-Object.entries(funcs).forEach(([name, func]) => {
-  console.log(
-    `${name}\t${func.in.type}\t${func.out.map((e) => e.type).join("/")}`
-  );
-});
+Object.entries(funcs)
+  .sort()
+  .forEach(([name, func]) => {
+    console.log(
+      [
+        name,
+        func.in.type,
+        func.out.map((e) => e.type).join("/"),
+        func.in.authLevel,
+      ].join("\t")
+    );
+  });
